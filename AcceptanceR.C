719744bec146ef47f3879321a762baae4278dea9
ryanrich7 <ryan.richards@stonybrook.edu>
2021-01-15 09:07:35 -0500

    Redid macro

--- AcceptanceR.C
+++ AcceptanceR.C
@@ -1,17 +1,17 @@
 
 //Author -- Ryan Richards
-//Looks at Qsq,Lab Phi, Cos(Phi),Sin(Phi) and theta,phi_tg for Main, A_T detectors
+////Looks at Qsq,Lab Phi, Cos(Phi),Sin(Phi) and theta,phi_tg for Main, A_T detectors
 
 //adc_up - main detector
 //adc_in - A_T in, adc_out - A_T out
-//Ebeam in GeV
-void AcceptanceR(int run, double th0, double adc_up, double adc_in, double adc_out, double Ebeam){ 
+//Ebeam in GeV - Run dependent...
+void AcceptanceL(int run, double th0, double adc_up, double adc_in, double adc_out, double Ebeam){ 
 
    gStyle->SetOptStat(1002211);
 
 
    TChain *T = new TChain("T");
-   T->Add(Form("/lustre19/expphy/volatile/halla/parity/crex_optics_rootfiles/prexRHRS_%d_-1.root",run));
+   T->Add(Form("/lustre19/expphy/volatile/halla/parity/crex_optics_rootfiles/prexRHRS_%d_-1*.root",run));
 
    double d2r = TMath::Pi()/180; 
    double r2d = 1/d2r;
@@ -29,7 +29,7 @@ void AcceptanceR(int run, double th0, double adc_up, double adc_in, double adc_o
    TH1F *ThTg[3],*PhTg[3];
    TH1F *Qsq[3];
 
-   TCanvas *c_q[3], *c_ph[3], *c_cos[3], *c_sin[3], *c_thtg[3], *c_phtg[3];
+   TCanvas *c_q, *c_ph, *c_cos, *c_sin, *c_thtg, *c_phtg;
 
 
    int color[3] = {2,3,4};
@@ -37,18 +37,26 @@ void AcceptanceR(int run, double th0, double adc_up, double adc_in, double adc_o
 
    for(int i = 0; i < 3; i++){ 
  
-      Qsq[i] = new TH1F(Form("Qsq[%i]",i),Form("RHRS Q^{2} for %s Detector, Run %d",det[i],run),150,0,0.035);
+      Qsq[i] = new TH1F(Form("Qsq[%i]",i),Form("RHRS Q^{2} for %s Detector, Run %d",det[i],run),150,0,0.07);
       //Relative to horizontal 
       Phi[i] = new TH1F(Form("Phi[%i]",i),Form("RHRS #Phi_{H} for %s Detector, Run %d",det[i],run),150,135,225);
      //Relative to vertical
      Cos[i] = new TH1F(Form("Cos[%i]",i),Form("RHRS Cos(#Phi_{V}) for %s Detector, Run %d",det[i],run),150,-0.8,0.8);
-     Sin[i] = new TH1F(Form("Sin[%i]",i),Form("RHRS Sin(#Ph_{V}) for %s Detector, Run %d",det[i],run),150,0.6,1.05);
+     Sin[i] = new TH1F(Form("Sin[%i]",i),Form("RHRS Sin(#Phi_{V}) for %s Detector, Run %d",det[i],run),150,0.6,1.05);
      //Thtg and Phtg
      ThTg[i] = new TH1F(Form("ThTg[%i]",i),Form("RHRS #theta_{tg} for %s Detector, Run %d",det[i],run),150,-0.06,0.06);
      PhTg[i] = new TH1F(Form("PhTg[%i]",i),Form("RHRS #phi_{tg} for %s Detector, Run %d",det[i],run),150,-0.03,0.03);
 
      Qsq[i]->SetLineColor(color[i]); Phi[i]->SetLineColor(color[i]); Cos[i]->SetLineColor(color[i]);
      Sin[i]->SetLineColor(color[i]); ThTg[i]->SetLineColor(color[i]); PhTg[i]->SetLineColor(color[i]);
+
+     Qsq[i]->GetXaxis()->SetTitle("Q^{2} (GeV/c)^{2}");
+     Sin[i]->GetXaxis()->SetTitle("Sin(#Phi_{V})");
+     Cos[i]->GetXaxis()->SetTitle("Cos(#Phi_{V})");
+     Phi[i]->GetXaxis()->SetTitle("#Phi_{H} (deg)");
+     ThTg[i]->GetXaxis()->SetTitle("#theta_{tg} (rad)");
+     PhTg[i]->GetXaxis()->SetTitle("phi_{tg} (rad)");
+
 
     }
 
@@ -97,7 +105,7 @@ void AcceptanceR(int run, double th0, double adc_up, double adc_in, double adc_o
       //CosPhi relative to vertical
       thisCosPhi = thisThdat/(TMath::Sqrt(thisThdat*thisThdat+sth0*sth0+2*tanth0*thisPhdat + thisPhdat*thisPhdat*secth0*secth0));     
       thisSinPhi = TMath::Sqrt(1-thisCosPhi*thisCosPhi);     
-      //Phi relative in horizontal
+      //Phi relative in horizontal -- Add 180 for RHRS
       thisPhi = 270 - r2d*TMath::ACos(thisCosPhi);
 
       if(thisupADC > adc_up){ 
@@ -146,35 +154,63 @@ void AcceptanceR(int run, double th0, double adc_up, double adc_in, double adc_o
 
 
 
-  for(int i = 0; i < 3; i++){
-   c_q[i] = new TCanvas(Form("c_q[%i]",i),Form("c_q[%i]",i));
+
+   c_q = new TCanvas();
+   c_q->Divide(2,2);
+   for(int i = 0; i < 3; i++){
+   c_q->cd(i+1);
    Qsq[i]->Draw("HIST");
-   c_q[i]->SaveAs(Form("qsqRHRS_%s_Run%d.pdf",det[i],run));
+   }
+   c_q->SaveAs(Form("qsqRHRS_Run%d.pdf",run));
 
 
-   c_cos[i] = new TCanvas(Form("c_cos[%i]",i),Form("c_cos[%i]",i));
+   c_cos = new TCanvas();
+   c_cos->Divide(2,2);
+   for(int i = 0; i < 3; i++){
+   c_cos->cd(i+1);
    Cos[i]->Draw("HIST");
-   c_cos[i]->SaveAs(Form("cosRHRS_%s_Run%d.pdf",det[i],run));
+   }
+   c_cos->SaveAs(Form("cosRHRS_Run%d.pdf",run));
   
-   c_sin[i] = new TCanvas(Form("c_sin[%i]",i),Form("c_sin[%i]",i));
+   c_sin = new TCanvas();
+   c_sin->Divide(2,2);
+   for(int i = 0; i < 3; i++){ 
+   c_sin->cd(i+1);
    Sin[i]->Draw("HIST");
-   c_sin[i]->SaveAs(Form("sinRHRS_%s_Run%d.pdf",det[i],run));
+   }
+   c_sin->SaveAs(Form("sinRHRS_Run%d.pdf",run));
 
-   c_ph[i] = new TCanvas(Form("c_ph[%i]",i),Form("c_ph[%i]",i));
+   c_ph = new TCanvas();
+   c_ph->Divide(2,2); 
+   for(int i = 0; i < 3; i++){
+   c_ph->cd(i+1);
    Phi[i]->Draw("HIST");
-   c_ph[i]->SaveAs(Form("phiRHRS_%s_Run%d.pdf",det[i],run));
+   }
+   c_ph->SaveAs(Form("phiRHRS_Run%d.pdf",run));
 
-   c_thtg[i] = new TCanvas(Form("c_thtg[%i]",i),Form("c_thtg[%i]",i));
+   c_thtg = new TCanvas();
+   c_thtg->Divide(2,2);
+   for(int i = 0; i < 3; i++){
+   c_thtg->cd(i+1);
    ThTg[i]->Draw("HIST");
-   c_thtg[i]->SaveAs(Form("thtgRHRS_%s_Run%d.pdf",det[i],run));
+   }
+   c_thtg->SaveAs(Form("thtgRHRS_Run%d.pdf",run));
 
-   c_phtg[i] = new TCanvas(Form("c_phtg[%i]",i),Form("c_phtg[%i]",i));
+   c_phtg = new TCanvas();
+   c_phtg->Divide(2,2);
+   for(int i = 0; i < 3; i++){
+   c_phtg->cd(i+1);
    PhTg[i]->Draw("HIST");
-   c_phtg[i]->SaveAs(Form("phtgRHRS_%s_Run%d.pdf",det[i],run));
+   }
+   c_phtg->SaveAs(Form("phtgRHRS_Run%d.pdf",run));
 
-   } 
 
 
+
+    
+
+
+    
 
 
 
